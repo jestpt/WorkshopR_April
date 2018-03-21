01731e62322c75d1dedf1ce808dd0f9a5e5b62aa
grifo <leitaogrifo@gmail.com>
2018-03-21 22:51:38 +0000

    Starting introduction

---
+++ Introduction/Introduction.R
@@ -0,0 +1,17 @@
+#Atribuir valores a variaveis
+x <- 9
+y <- 2
+
+
+#Operadores aritmeticos
+x+y               #adição
+x-y               #subtração
+x*y               #multiplicação
+x/y               #divisão
+x^y   #or x**y    expoente
+x%%y              #resto da divisão
+x%/%y             #arrondamento por defeito
+
+#Print variable
+x
+print(x)
